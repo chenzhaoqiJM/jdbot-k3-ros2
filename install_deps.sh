2e9f7f546714f402ee07660e5b348c555363c961
chenzhaoqi <zhaoqi.chen@spacemit.com>
2026-04-23 14:10:57 +0800

    Update deps

--- install_deps.sh
+++ install_deps.sh
@@ -4,6 +4,13 @@ sudo apt install -y python3-zmq \
 libspdlog-dev libconsole-bridge-dev liborocos-kdl-dev nlohmann-json3-dev liblgpio-dev \
 liblttng-ust-dev libgpiod-dev
 
+sudo apt install ros-dev-tools ros-humble-ros-base \
+python3-numpy 'ros-humble-cartographer*' 'ros-humble-nav*' libpcap-dev libuvc-dev \
+ros-humble-filters ros-humble-turtlesim ros-humble-camera-info-manager  ros-humble-pcl-ros \
+ros-humble-image-common ros-humble-image-geometry ros-humble-robot-localization \
+ros-humble-joint-state-publisher \
+ros-humble-tf-transformations
+
 git clone https://github.com/YDLIDAR/YDLidar-SDK.git
 cd YDLidar-SDK
 mkdir build && cd build
